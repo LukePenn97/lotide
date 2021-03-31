4036cf08a8787f1f27f1c6601dd9648f6ded8830
Luke Penner <lukepenner@hotmail.ca>
2021-03-31 15:50:58 -0700

    added takeUntil

---
+++ takeUntil.js
@@ -0,0 +1,42 @@
+const takeUntil = function(array, callback) {
+  let result = [];
+  for (const elem of array) {
+    if (!callback(elem)) {
+      result.push(elem);
+    } else {
+      return result;
+    }
+  }
+  return result;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🆘🆘🆘 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (const index in array1) {
+      if (array1[index] !== array2[index]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [1,2,5,7,2]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
