05b8436aebf32678a9dccabcf243b3d0a3028afc
Luke Penner <lukepenner@hotmail.ca>
2021-04-02 13:23:45 -0700

    updated eqArrays with recursion

--- eqArrays.js
+++ eqArrays.js
@@ -7,18 +7,27 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
+  //console.log(array1,array2);
+  let result = true;
   if (array1.length !== array2.length) {
-    return false;
+    result = false;
   } else {
     for (const index in array1) {
-      if (array1[index] !== array2[index]) {
-        return false;
+      if (Array.isArray(array1[index]) && Array.isArray(array2[index])) {
+        result = eqArrays(array1[index],array2[index]);
+      } else if (array1[index] !== array2[index]) {
+        result = false;
       }
     }
   }
-  return true;
+  return result;
 };
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([3, 2, 3], [1, 2, 3]), false);
 assertEqual(eqArrays([99, 2, 3.1], [99, 2, 3.1]), true);
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]),true); // => true
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]),false); // => false
