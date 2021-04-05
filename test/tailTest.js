b787fae9cb7685c6f7f010c0f843e74229c635be
Luke Penner <lukepenner@hotmail.ca>
2021-04-04 22:45:54 -0700

    modified tailTest to use mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,11 @@
-const assertArraysEqual = require('../assertArraysEqual.js');
+const assert = require('chai').assert;
 const tail = require('../tail.js');
 
-assertArraysEqual(tail([1,2,3,4,5]),[2,3,4,5]);
-assertArraysEqual(tail([21,22,23]),[22,23]);
+describe("#head", () => {
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.deepEqual(tail([1,2,3,4,5]),[2,3,4,5]);
+  });
+  it("returns Hello for [Hello, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail([21,22,23]),[22,23]);
+  });
+});
